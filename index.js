ab6d0bb50e855b8f0c9c1ab005c31b3cdd3cba2a
Michael Mainguy <mike.mainguy@gmail.com>
2023-01-21 15:34:20 -0600

    Added new service work and manifest code for PWA.

--- index.js
+++ index.js
@@ -49,18 +49,54 @@ if (env.NODE_ENV != 'development') {
  */
 app.use('/dist', express.static('client/dist'));
 app.use('/assets', express.static('client/assets'));
+app.use('/sw.js', express.static('client/serviceworker/sw.js'));
 app.use('/favicon.ico', express.static('client/favicon.ico'));
 app.get('/manifest.webmanifest', (req, res) => {
     res.send(
         `
-        {"name": "Immersive Idea",
-        "display": "standalone",
-        "start_url": "${env.AUTH0_BASE_URL}",
-        "scope": "${env.AUTH0_BASE_URL}",
-        "short_name": "Immersive Idea",
-        "theme_color": "#000000",
-        "background_color": "#000000",
-        "description": "A diagramming tool to help collaborate and edit 3d diagrams in an immersive environment."}
+        {
+  "name": "Immersive Idea",
+  "display": "standalone",
+  "start_url": "/",
+  "scope": "${env.AUTH0_BASE_URL}",
+  "short_name": "Immersive Idea",
+  "theme_color": "#000000",
+  "background_color": "#000000",
+  "description": "A diagramming tool to help collaborate and edit 3d diagrams in an immersive environment.",
+  "icons": [
+    {
+      "src": "/assets/android-icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    }
+  ],
+  "categories": [
+    "utilities",
+    "business",
+    "education",
+    "productivity"
+  ],
+  "screenshots": [
+    {
+      "src": "/assets/com.oculus.browser-20230121-110512.jpg",
+      "sizes": "1024x1024",
+      "type": "image/jpeg",
+      "label": "Example 1"
+    },
+    {
+      "src": "/assets/com.oculus.browser-20230121-110747.jpg",
+      "sizes": "1024x1024",
+      "type": "image/jpeg",
+      "label": "Example 2"
+    },
+    {
+      "src": "/assets/com.oculus.browser-20230121-111323.jpg",
+      "sizes": "1024x1024",
+      "type": "image/jpeg",
+      "label": "Complex Architecture Diagram"
+    }
+  ]
+}
         `
     );
 });
